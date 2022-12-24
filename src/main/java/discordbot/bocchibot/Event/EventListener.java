7d7158abf7fd05265638124b804267a722b3b393
Fikri Aufaa Zain <fikri.azain@gmail.com>
2022-12-24 16:09:36 +0700

    add EventListener to the program

---
+++ src/main/java/discordbot/bocchibot/Event/EventListener.java
@@ -0,0 +1,15 @@
+package discordbot.bocchibot.Event;
+import discord4j.core.event.domain.Event;
+import reactor.core.publisher.Mono;
+
+public interface EventListener <T extends Event> {
+
+    Class<T> getEventType();
+    Mono<Void> execute(T event);
+
+    default Mono<Void> handleError(Throwable error) {
+        error.printStackTrace();
+        return Mono.empty();
+
+    }
+}
