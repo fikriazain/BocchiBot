f9470b5ec0485734b0a6023082ede38146e9e485
Fikri Aufaa Zain <fikri.azain@gmail.com>
2022-12-24 15:43:51 +0700

    initial commit

---
+++ src/main/java/discordbot/bocchibot/BocchibotApplication.java
@@ -0,0 +1,13 @@
+package discordbot.bocchibot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BocchibotApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BocchibotApplication.class, args);
+    }
+
+}
---
+++ src/test/java/discordbot/bocchibot/BocchibotApplicationTests.java
@@ -0,0 +1,13 @@
+package discordbot.bocchibot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BocchibotApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
